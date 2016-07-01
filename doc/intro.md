481ca7f4c004820f9ca2e4766fbbfb83bd3f84c9
André Poleza <andrepoleza@nepente.io>
2016-07-01 13:41:27 -0300

    :rocket:

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to church-encoding-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/church_encoding_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns church-encoding-clj.core-test
+  (:require [clojure.test :refer :all]
+            [church-encoding-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
