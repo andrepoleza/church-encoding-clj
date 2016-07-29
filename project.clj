0cd277073009f8edae22fd81504166b197a321f7
André Poleza <andrepoleza@nepente.io>
2016-07-29 16:29:34 -0300

    :rocket: 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject church-encoding-clj "0.1.0-SNAPSHOT"
+(defproject church-encoding-clj "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
