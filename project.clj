2616e14b8da7a7b7ea39e916c79d829d6ad263cf
André Poleza <andrepoleza@gmail.com>
2017-02-07 14:10:09 -0200

    Fulfills :description at project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject church-encoding-clj "1.0.0"
-  :description "FIXME: write description"
+  :description "A naive Church Encoding implementation"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
